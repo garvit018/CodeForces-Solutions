5d70b5b0e9ec664f9177424f04554a4603f45885
Garvit Goyal <garvitgoyal83@gmail.com>
2025-06-02 20:10:03 +0530

    initial commit

---
+++ 2091E.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vector<ll>>
+#define vs vector<string>
+#define umapii unordered_map<int, int>
+#define vpi vector<pair<int, int>>
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+constexpr int INF = 1e9;
+constexpr ll INFLL = 1e18;
+constexpr int MOD = 1e9 + 7;
+
+// bool isPrime(ll n) {
+//     if (n < 2) return false;
+//     if (n < 4) return true;
+//     if (n % 2 == 0 || n % 3 == 0) return false;
+//     for (ll i = 5; i * i <= n; i += 6) {
+//         if (n % i == 0 || n % (i + 2) == 0) return false;
+//     }
+//     return true;
+// }
+
+// ll gcd(ll a, ll b) {
+//     return b == 0 ? a : gcd(b, a % b);
+// }
+
+// ll lcm(ll a, ll b) {
+//     return (a / gcd(a, b)) * b;
+// }
+
+/*
+
+*/
+
+vector<bool> SieveOfEratosthenes(int n) // GFG
+{
+    vector<bool> prime(n + 1, true);
+    for (int p = 2; p * p <= n; p++)
+    {
+        if (prime[p] == true)
+        {
+            for (int i = p * p; i <= n; i += p)
+            {
+                prime[i] = false;
+            }
+        }
+    }
+    return prime;
+}
+
+vector<bool> prime = SieveOfEratosthenes(1e7 + 1);
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll sum = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        if (prime[i])
+        {
+            sum += (n / i);
+        }
+    }
+    cout << sum << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
