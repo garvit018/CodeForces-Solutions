a161d00126f8b7a62429e4ce6344c00e16415264
Garvit Goyal <garvitgoyal83@gmail.com>
2025-06-02 22:24:50 +0530

    initial commit

---
+++ 1703E.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vector<ll>>
+#define vs vector<string>
+#define umapii unordered_map<int, int>
+#define vpi vector<pair<int, int>>
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+constexpr int INF = 1e9;
+constexpr ll INFLL = 1e18;
+constexpr int MOD = 1e9 + 7;
+
+// bool isPrime(ll n) {
+//     if (n < 2) return false;
+//     if (n < 4) return true;
+//     if (n % 2 == 0 || n % 3 == 0) return false;
+//     for (ll i = 5; i * i <= n; i += 6) {
+//         if (n % i == 0 || n % (i + 2) == 0) return false;
+//     }
+//     return true;
+// }
+
+// ll gcd(ll a, ll b) {
+//     return b == 0 ? a : gcd(b, a % b);
+// }
+
+// ll lcm(ll a, ll b) {
+//     return (a / gcd(a, b)) * b;
+// }
+
+/*
+0 deg -> i,j
+90 deg -> j,n-i-1
+180 deg -> n-i-1,n-j-1
+270 deg -> n-j-1,i
+min (ones,zeros)
+*/
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    char arr[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    ll ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            ll ones = 0;
+            if (arr[i][j] == '1')
+            {
+                ones++;
+            }
+            if (arr[j][n - i - 1] == '1')
+            {
+                ones++;
+            }
+            if (arr[n - i - 1][n - j - 1] == '1')
+            {
+                ones++;
+            }
+            if (arr[n - j - 1][i] == '1')
+            {
+                ones++;
+            }
+            ans += min(ones, 4 - ones);
+        }
+    }
+    cout << ans / 4 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
