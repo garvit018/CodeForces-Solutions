b8acd7c88b26dbda0c384ca2fd393c9e2471130d
Garvit Goyal <garvitgoyal83@gmail.com>
2025-06-07 14:59:39 +0530

    initial commit

---
+++ 1881A.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vector<ll>>
+#define vs vector<string>
+#define umapii unordered_map<int, int>
+#define vpi vector<pair<int, int>>
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+constexpr int INF = 1e9;
+constexpr ll INFLL = 1e18;
+constexpr int MOD = 1e9 + 7;
+
+// bool isPrime(ll n) {
+//     if (n < 2) return false;
+//     if (n < 4) return true;
+//     if (n % 2 == 0 || n % 3 == 0) return false;
+//     for (ll i = 5; i * i <= n; i += 6) {
+//         if (n % i == 0 || n % (i + 2) == 0) return false;
+//     }
+//     return true;
+// }
+
+// ll gcd(ll a, ll b) {
+//     return b == 0 ? a : gcd(b, a % b);
+// }
+
+// ll lcm(ll a, ll b) {
+//     return (a / gcd(a, b)) * b;
+// }
+
+/*
+
+*/
+
+void solve()
+{
+    ll n, m;
+    string x, s;
+    cin >> n >> m >> x >> s;
+    for (int i = 0; i < 6; i++)
+    {
+        if (x.find(s) != x.npos)
+        {
+            cout << i << endl;
+            return;
+        }
+        x += x;
+    }
+    cout << -1 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
