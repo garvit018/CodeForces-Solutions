ad6e6ca42ca60619b54e611dffc0c920569d7a27
Garvit Goyal <garvitgoyal83@gmail.com>
2025-06-17 14:22:37 +0530

    initial commit

---
+++ 1734C.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vector<ll>>
+#define vs vector<string>
+#define umapii unordered_map<int, int>
+#define vpi vector<pair<int, int>>
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+constexpr int INF = 1e9;
+constexpr ll INFLL = 1e18;
+constexpr int MOD = 1e9 + 7;
+
+// bool isPrime(ll n) {
+//     if (n < 2) return false;
+//     if (n < 4) return true;
+//     if (n % 2 == 0 || n % 3 == 0) return false;
+//     for (ll i = 5; i * i <= n; i += 6) {
+//         if (n % i == 0 || n % (i + 2) == 0) return false;
+//     }
+//     return true;
+// }
+
+// ll gcd(ll a, ll b) {
+//     return b == 0 ? a : gcd(b, a % b);
+// }
+
+// ll lcm(ll a, ll b) {
+//     return (a / gcd(a, b)) * b;
+// }
+
+/*
+
+*/
+
+void solve()
+{
+    ll n;
+    string s;
+    cin >> n >> s;
+    ll cost = 0;
+    vector<bool> vis(n, false);
+    for (int i = 0; i < n; i++)
+    {
+        ll idx = i;
+        while (idx < n && (s[idx] == '0' || vis[idx]))
+        {
+            if (vis[idx])
+            {
+                idx += (i + 1);
+                continue;
+            }
+            cost += (i + 1);
+            vis[idx] = true;
+            idx += (i + 1);
+        }
+    }
+    cout << cost << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ 2113A.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vector<ll>>
+#define vs vector<string>
+#define umapii unordered_map<int, int>
+#define vpi vector<pair<int, int>>
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+constexpr int INF = 1e9;
+constexpr ll INFLL = 1e18;
+constexpr int MOD = 1e9 + 7;
+
+// bool isPrime(ll n) {
+//     if (n < 2) return false;
+//     if (n < 4) return true;
+//     if (n % 2 == 0 || n % 3 == 0) return false;
+//     for (ll i = 5; i * i <= n; i += 6) {
+//         if (n % i == 0 || n % (i + 2) == 0) return false;
+//     }
+//     return true;
+// }
+
+// ll gcd(ll a, ll b) {
+//     return b == 0 ? a : gcd(b, a % b);
+// }
+
+// ll lcm(ll a, ll b) {
+//     return (a / gcd(a, b)) * b;
+// }
+
+/*
+k-nx<a -> n dishes
+n = k-a/x +1
+*/
+
+void solve()
+{
+    ll k, a, b, x, y;
+    cin >> k >> a >> b >> x >> y;
+    if (x > y)
+    {
+        swap(x, y);
+        swap(a, b);
+    }
+    ll ans = 0;
+    if (k - a >= 0)
+    {
+        ans += (k - a) / x + 1;
+        k -= x * ans;
+    }
+    if (k - b >= 0)
+    {
+        ans += (k - b) / y + 1;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
